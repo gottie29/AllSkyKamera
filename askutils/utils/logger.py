33a9ee9e64dd2c5d6dbba04f43db1bf176b0130d
gottie29 <gottie@web.de>
2025-10-05 20:01:42 +0200

    Fix: logger.py

--- askutils/utils/logger.py
+++ askutils/utils/logger.py
@@ -3,14 +3,14 @@ import datetime
 def log(message):
     """Gibt eine normale Lognachricht mit Zeitstempel aus."""
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[INFO]  {timestamp} – {message}")
+    print(f"[INFO]  {timestamp} - {message}")
 
 def warn(message):
     """Gibt eine Warnung mit Zeitstempel aus."""
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[WARN]  {timestamp} – {message}")
+    print(f"[WARN]  {timestamp} - {message}")
 
 def error(message):
     """Gibt eine Fehlermeldung mit Zeitstempel aus."""
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[ERROR] {timestamp} – {message}")
+    print(f"[ERROR] {timestamp} - {message}")
