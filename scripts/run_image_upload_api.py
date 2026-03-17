6471d061bbdbf6b52da292f9f4b9332800f674d8
gottie29 <gottie@web.de>
2026-03-17 10:34:06 +0100

    liitle bugfix

--- scripts/run_image_upload_api.py
+++ scripts/run_image_upload_api.py
@@ -4,3 +4,6 @@ from askutils.uploader.image_upload_api import upload_image_api
 
 if __name__ == "__main__":
     upload_image_api()
+    
+def upload_image_api(date=None):
+    return upload_nightly_batch(date)
